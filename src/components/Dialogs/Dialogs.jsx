d633a08c086219c795eb84d038f7466bc2aca44a
ypen <ypen1691@gmail.com>
2020-04-03 10:41:41 +0300

    change array and mao function added to  dialogs and posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,20 +18,33 @@ const Message = (props) => {
 };
 
 const  Dialogs = (props) => {
+    let dialogsData =[
+        {   ids: 1,   name: 'John'  },
+        {   ids: 2,   name: 'Alex' },
+        {   ids:3,    name: 'Sofia' },
+        {   ids:3,    name: 'Said' },
+        {   ids:3,    name: 'Hugo' }
+    ];
+    let messagesData =[
+        {   ids: 1,   msg: 'Hey'  },
+        {   ids: 2,   msg: 'Hi' },
+        {   ids:3,    msg: 'Hello' },
+        {   ids:4,    msg: 'Hello' },
+        {   ids:5,    msg: 'Hello123' }
+    ];
+    let dialog_mas = dialogsData.map( dialog => <DialogItem name={dialog.name} ids={dialog.ids} />);
+    let message_mas = messagesData.map( msg => <Message title_m={msg.msg} ids={msg.ids} /> );
+
+
     return (
         <div>
             <h1>Dialogs</h1>
             <div className={ds.dialogs}>
                 <div className={ds.dialogs_items}>
-                    <DialogItem name='John' ids='1'/>
-                    <DialogItem name='Alex' ids='2'/>
-                    <DialogItem name='Sofia' ids='3'/>
+                    {dialog_mas}
                 </div>
                 <div className={ds.messages}>
-                    <Message title_m='hey'/>
-                    <Message title_m='hi'/>
-                    <Message title_m='hello'/>
-
+                    {message_mas}
                 </div>
             </div>
         </div>
--- src/components/Profile/Mypost/Mypost.jsx
+++ src/components/Profile/Mypost/Mypost.jsx
@@ -3,6 +3,15 @@ import cProf from './Mypost.module.css';
 import Post from "./Post/Post";
 
 const Mypost = () => {
+    let postsData =[
+        {   ids: 1,   msg: 'test 1', likes: 12  },
+        {   ids: 2,   msg: 'Test 2', likes: 10 },
+        {   ids:3,    msg: 'Test 3', likes: 11 },
+        {   ids:3,    msg: 'Test 4', likes: 8 },
+        {   ids:3,    msg: 'Test 5', likes: 24 }
+    ];
+    let postItem = postsData.map( p => <Post ids={p.ids} msg={p.msg} likes={p.likes} /> );
+
     return (
             <div className={cProf.block_post}>
                 <h4>My Posts</h4>
@@ -12,11 +21,7 @@ const Mypost = () => {
                 </div>
                 <h4>Last Posts</h4>
                 <div className="posts">
-                    <Post msg='Hey,hi, hello' likes="34"/>
-                    <Post msg='test post second hello' likes="234"/>
-                    <Post msg='test 3' likes="342"/>
-                    <Post msg='test 4' likes="354"/>
-                    <Post msg='test 5' likes="934"/>
+                    {postItem}
                 </div>
             </div>
     )
